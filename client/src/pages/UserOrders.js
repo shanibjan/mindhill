d8e93e1a798f01031d483ac84f49d22539e5aa77
SHANIBJAN <shanib@SHANIBJANs-MacBook-Pro.local>
2024-10-02 03:02:43 -0700

    responsive completed

--- client/src/pages/UserOrders.js
+++ client/src/pages/UserOrders.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { motion, AnimatePresence } from "framer-motion";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import {
-  
   faSearch,
   faShoppingBasket,
   faTimes,
@@ -24,6 +23,7 @@ const UserOrders = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const userId = user ? user._id : null;
   const userName = user ? user.name : null;
+  const inputRef = useRef(null);
   const fetchorder = async () => {
     const res = await axios.get(`api/v1/product/get-order/${userId}`);
     setOrder(res.data);
@@ -66,10 +66,16 @@ const UserOrders = () => {
 
     nav("/search", { state: searchQuery });
   };
+
+  useEffect(() => {
+    if (isSearchVisible && inputRef.current) {
+      inputRef.current.focus(); // Focus on the input when it is displayed
+    }
+  }, [isSearchVisible]);
   return (
-    <div className="mt-[10%]">
+    <div className="mt-[10%] max-[1200px]:mt-[14%] max-[650px]:mt-[20%] max-[600px]:mt-[35%] max-[400px]:mt-[43%]">
       <div
-        className="overflow-visible py-[3%] px-[4%] bg-cover bg fixed w-full mt-[-10%] z-10 "
+        className="overflow-visible py-[3%] px-[4%] bg-cover bg fixed w-full mt-[-10%] max-[1200px]:mt-[-14%] max-[650px]:mt-[-20%] max-[600px]:mt-[-35%] max-[400px]:mt-[-43%]  z-10 "
         style={{
           background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${productOverView}) center/cover`,
         }}
@@ -84,54 +90,64 @@ const UserOrders = () => {
                 transition={{ duration: 0.5 }}
                 className="absolute w-full z-[200]"
               >
-                <div className="flex items-center bg-white px-[3%]">
+                <div className="flex justify-between items-center bg-white px-[3%]">
                   <input
+                    ref={inputRef}
                     type="text"
                     placeholder="Search products"
                     value={searchQuery}
                     onChange={(e) => setsearchQuery(e.target.value)}
-                    className="w-[90%] h-[70px]  text-[20px] text-[#244262] font-gorditaRegular outline-none"
+                    className="w-[90%] max-[800px]:w-[75%] max-[500px]:w-[70%] h-[70px]  text-[20px] max-[550px]:text-[15px]  text-[#244262] font-gorditaRegular outline-none"
                   />
 
                   <div
                     onClick={searchProducts}
-                    className="bg-[#244262] rounded-[50%] w-[40px] h-[40px] flex justify-center items-center text-white"
+                    className="bg-[#244262] rounded-[50%] w-[40px] h-[40px] max-[550px]:w-[30px] max-[550px]:h-[30px] flex justify-center items-center text-white"
                   >
-                    <FontAwesomeIcon icon={faSearch} className="h-[23px]" />
+                    <FontAwesomeIcon
+                      icon={faSearch}
+                      className="h-[23px] max-[550px]:h-[15px]"
+                    />
                   </div>
 
                   <div
                     onClick={() => setIsSearchVisible((prev) => !prev)}
-                    className="bg-[#244262] rounded-[50%] w-[40px] h-[40px] flex justify-center items-center text-white ml-[3%]"
+                    className="bg-[#244262] rounded-[50%] w-[40px] h-[40px] max-[550px]:w-[30px] max-[550px]:h-[30px]  flex justify-center items-center text-white ml-[3%]"
                   >
-                    <FontAwesomeIcon icon={faTimes} className="h-[23px]" />
+                    <FontAwesomeIcon
+                      icon={faTimes}
+                      className="h-[23px] max-[550px]:h-[15px]"
+                    />
                   </div>
                 </div>
               </motion.div>
             )}
           </AnimatePresence>
 
-          <div className="flex justify-between text-white items-center">
+          <div className="flex justify-between text-white items-center max-[600px]:block">
             <div onClick={() => nav("/")}>
-              <h2 className="font-AbrilRegular text-[40px] cursor-pointer">
+              <h2 className="font-AbrilRegular text-[40px] cursor-pointer max-[600px]:mb-[6%]">
                 MIND HILL
               </h2>
             </div>
-            <div className="flex justify-between w-[30%] max-lg:w-[50%] items-center font-gorditaMedium cursor-pointer">
+            <div className="flex justify-between w-[30%] max-lg:w-[50%] max-[600px]:w-full items-center font-gorditaMedium cursor-pointer">
               <div
                 onClick={janu}
-                className="bg-[#FFA27E] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center"
+                className="bg-[#FFA27E] rounded-[50%] w-[50px] h-[50px] max-[550px]:w-[40px] max-[550px]:h-[40px] flex justify-center items-center"
               >
-                <FontAwesomeIcon icon={faSearch} className="h-[23px]" />
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  className="h-[23px] max-[550px]:h-[15px]"
+                />
               </div>
 
               <div
                 onClick={() => {
                   user ? nav("/wishlist") : window.alert("Please Login");
                 }}
-                className="bg-[#244262] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center relative"
+                className="bg-[#244262] rounded-[50%] w-[50px] h-[50px] max-[550px]:w-[40px] max-[550px]:h-[40px] flex justify-center items-center relative"
               >
-                <FontAwesomeIcon icon={faHeart} className="h-[23px]" />
+                <FontAwesomeIcon icon={faHeart} className="h-[23px] max-[550px]:h-[15px]" />
                 <h1 className="absolute right-[-15%] bottom-[-20%] bg-white text-[#244262] w-[25px] h-[25px] rounded-[50%] p-[5%]">
                   {user ? wishlist.length : "0"}
                 </h1>
@@ -140,16 +156,16 @@ const UserOrders = () => {
                 onClick={() => {
                   user ? nav("/cart") : window.alert("Please Login");
                 }}
-                className="bg-[#94C4F7] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center relative"
+                className="bg-[#94C4F7] rounded-[50%] w-[50px] h-[50px] max-[550px]:w-[40px] max-[550px]:h-[40px] flex justify-center items-center relative"
               >
-                <FontAwesomeIcon icon={faShoppingBasket} className="h-[23px]" />
+                <FontAwesomeIcon icon={faShoppingBasket} className="h-[23px] max-[550px]:h-[15px]" />
                 <h1 className="absolute right-[-15%] bottom-[-20%] bg-white text-[#244262] w-[25px] h-[25px] rounded-[50%] p-[5%]">
                   {user ? data.length : "0"}
                 </h1>
               </div>
               <div className="dropdown">
                 {user ? (
-                  <div className="rounded-[50%] w-[50px] h-[50px]">
+                  <div className="rounded-[50%] w-[50px] h-[50px] max-[550px]:w-[40px] max-[550px]:h-[40px] ">
                     <img
                       className="h-full w-full object-cover rounded-[50%]"
                       src={user.profile}
@@ -157,12 +173,12 @@ const UserOrders = () => {
                     />
                   </div>
                 ) : (
-                  <div className="bg-[#FFA27E] rounded-[50%] w-[50px] h-[50px] flex justify-center items-center ">
-                    <FontAwesomeIcon icon={faUser} className="h-[23px]" />
+                  <div className="bg-[#FFA27E] rounded-[50%] w-[50px] h-[50px] max-[550px]:w-[40px] max-[550px]:h-[40px] flex justify-center items-center ">
+                    <FontAwesomeIcon icon={faUser} className="h-[23px] max-[550px]:h-[15px]" />
                   </div>
                 )}
 
-                <ul className="dropdown-menu text-[#244262] leading-[35px] ">
+                <ul className="dropdown-menu text-[#244262] leading-[35px] max-[1000px]:px-[20px] max-[1000px]:left-[-60px] max-[550px]:text-[11px] max-[500px]:left-[-80px] max-[1000px]:w-[150px] ">
                   <li
                     onClick={() => {
                       localStorage.removeItem("user");
@@ -186,7 +202,7 @@ const UserOrders = () => {
         </div>
       </div>
 
-      <h1 className="font-AbrilRegular text-[#244262] text-[40px] my-[5%]">
+      <h1 className="font-AbrilRegular text-[#244262] text-[40px]  max-[550px]:text-[30px] max-[370px]:text-[25px]  my-[5%]">
         User Orders
       </h1>
       <div className="px-[3%]">
@@ -194,9 +210,9 @@ const UserOrders = () => {
           <h2 className="font-gorditaMedium">User:{userName}</h2>
           <div>
             <div>
-              <div className="flex justify-between text-left py-[1%] text-[20px] font-AbrilRegular text-[#244262] items-center ">
-                <div className="w-[5%]"></div>
-                <div className="w-[10%]"></div>
+              <div className="flex justify-between text-left py-[1%] text-[20px] max-[550px]:text-[15px] max-[470px]:text-[13px] font-AbrilRegular text-[#244262] items-center ">
+                <div className="w-[5%] max-[550px]:w-[2%]"></div>
+                <div className="w-[10%] max-[550px]:hidden"></div>
                 <div className="w-[25%]">
                   <h1>Product</h1>
                 </div>
@@ -213,15 +229,15 @@ const UserOrders = () => {
               {order.map((items, index) => {
                 return (
                   <div className=" border-[1px] border-gray-300 shadow-lg mb-[4%]">
-                    <h1 className="text-left font-AbrilRegular text-[#244262] text-[30px] p-[2%]">
+                    <h1 className="text-left font-AbrilRegular text-[#244262] text-[30px] max-[550px]:text-[20px] p-[2%]">
                       Order No:{index + 1}
                     </h1>
 
                     {items.product.map((item) => {
                       return (
-                        <div className="flex justify-between text-left py-[1%] text-[16px] font-gorditaRegular text-[#244262] items-center border-y-[1px] border-y-gray-300 h-[115px]">
-                          <div className="w-[5%]"></div>
-                          <div className="w-[10%] p-[1%]">
+                        <div className="flex justify-between text-left py-[1%] text-[16px] max-[550px]:text-[13px] max-[470px]:text-[12px] font-gorditaRegular text-[#244262] items-center border-y-[1px] border-y-gray-300 h-[115px] max-[550px]:h-[65px]">
+                          <div className="w-[5%] max-[550px]:w-[2%]"></div>
+                          <div className="w-[10%] p-[1%] max-[550px]:hidden">
                             <img src={item.img1} alt="" />
                           </div>
                           <div className="w-[25%]">
@@ -232,7 +248,7 @@ const UserOrders = () => {
                           </div>
                           <div className="w-[20%]">
                             <div className="flex items-center h-[54px] ">
-                              <div className="py-[15px] px-[25px] bg-[#EBF5FF] font-gorditaRegular">
+                              <div className="py-[15px] px-[25px] max-[550px]:py-[12px] max-[550px]:px-[20px] max-[400px]:py-[10px] max-[400px]:px-[15px] bg-[#EBF5FF] font-gorditaRegular">
                                 <h3>{item.quantity}</h3>
                               </div>
                             </div>
@@ -244,14 +260,14 @@ const UserOrders = () => {
                       );
                     })}
 
-                    <div className="flex justify-evenly font-gorditaMedium my-[5%] ">
-                      <div className="w-[30%] text-left">
+                    <div className="flex justify-evenly font-gorditaMedium my-[5%] max-[550px]:text-[12px] max-[400px]:text-[10px] max-[500px]:block  ">
+                      <div className="w-[30%] text-left max-[500px]:text-center max-[500px]:w-full"> 
                         <h2>Delivery Address:</h2>
                         <div className="font-gorditaRegular">
                           <p>{items.address}</p>
                         </div>
                       </div>
-                      <h2>Status:{items.status}</h2>
+                      <h2 className="max-[500px]:my-[4%]" >Status:{items.status}</h2>
 
                       <div>
                         <h2>Total bill: ₹{items.bill} /-</h2>
