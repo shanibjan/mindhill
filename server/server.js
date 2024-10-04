0666b664887abaf40ca6e24ac96a15ed5fd474e4
SHANIBJAN <shanib@SHANIBJANs-MacBook-Pro.local>
2024-10-04 01:56:43 -0700

    client and server folders

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.json({ limit: '10mb' }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'client', 'public')));
+
 
 
 app.use('/api/v1/auth',authRoutes);
