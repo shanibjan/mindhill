bf71608fe06fb76d81b29f625b8e82ea45f81a17
SHANIBJAN <shanib@SHANIBJANs-MBP.domain.name>
2024-10-07 01:50:43 -0700

    client and sesra

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import ProductOverView from "./pages/ProductOverView";
@@ -8,57 +8,54 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Admin from "./pages/Admin";
 import AdminLogin from "./pages/AdminLogin";
-import UserOrdersForAdmin from './pages/UserOrdersForAdmin';
+import UserOrdersForAdmin from "./pages/UserOrdersForAdmin";
 import UserOrders from "./pages/UserOrders";
 import Search from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route Component={Home} exact path="/" />
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
 
-        <Routes>
-          <Route Component={ProductOverView} path="/overview" />
-        </Routes>
+      <Routes>
+        <Route Component={ProductOverView} path="/overview" />
+      </Routes>
 
-        <Routes>
-          <Route Component={Cart} path="/cart" />
-        </Routes>
+     
 
-        <Routes>
-          <Route Component={WishList} path="/wishlist" />
-        </Routes>
+      <Routes>
+        <Route Component={WishList} path="/wishlist" />
+      </Routes>
 
-        <Routes>
-          <Route Component={Login} path="/login" />
-        </Routes>
-        <Routes>
-          <Route Component={Signup} path="/signup" />
-        </Routes>
+      <Routes>
+        <Route Component={Login} path="/login" />
+      </Routes>
+      <Routes>
+        <Route Component={Signup} path="/signup" />
+      </Routes>
 
-        <Routes>
-          <Route Component={AdminLogin} path="/admin_login" />
-        </Routes>
+      <Routes>
+        <Route Component={AdminLogin} path="/admin_login" />
+      </Routes>
 
-        <Routes>
-          <Route Component={Admin} path="/admin" />
-        </Routes>
+      <Routes>
+        <Route Component={Admin} path="/admin" />
+      </Routes>
 
-        <Routes>
-          <Route Component={UserOrdersForAdmin} path="/user_orders_for_admin" />
-        </Routes>
+      <Routes>
+        <Route Component={UserOrdersForAdmin} path="/user_orders_for_admin" />
+      </Routes>
 
-        <Routes>
-          <Route Component={UserOrders} path="/user_orders" />
-        </Routes>
+      <Routes>
+        <Route Component={UserOrders} path="/user_orders" />
+      </Routes>
 
-        <Routes>
-          <Route Component={Search} path="/search" />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route Component={Search} path="/search" />
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Post from './store/postContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter>
     <Post>
-
-    <App />
+      <App />
     </Post>
-  </React.StrictMode>
+  </BrowserRouter>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
