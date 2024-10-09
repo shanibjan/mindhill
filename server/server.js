bf6dddc3037a213bfd224d8461c0bd4c2236ec84
SHANIBJAN <shanib@SHANIBJANs-MacBook-Pro.local>
2024-10-09 00:33:33 -0700

    product overvieww

--- server/server.js
+++ server/server.js
@@ -45,6 +45,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
 
 
 
